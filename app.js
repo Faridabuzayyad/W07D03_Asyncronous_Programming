0d299a6f280f97064c55309ae14390500c7ca583
Faridabuzayyad <farid.abuzayyad@gmail.com>
2021-05-12 00:14:49 +0300

    Pulse Check Q2 - Done

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const fs = require("fs");
 const axios = require("axios");
+
+const readFile = () => {
+    fs.readFile('./data.txt' , (err , data)=>{
+        if(err){
+            console.log("error")
+        }
+        console.log('Data:' , data.toString());
+    })
+  };
+
